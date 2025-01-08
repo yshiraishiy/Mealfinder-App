6f32c6bc5b9385edd0a335cf7ca7c17bb9988fdf
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-08 12:43:40 +0900

    ✨単一の食事ページを表示

--- src/main.js
+++ src/main.js
@@ -20,7 +20,6 @@ function searchMeal(e) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         resultHeading.innerHTML = `<h2>Search result for '${term}':</h2>`;
 
         if (data.meals === null) {
@@ -48,5 +47,69 @@ function searchMeal(e) {
   }
 }
 
+// IDで食事の情報を取得
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      addMealToDom(meal);
+    });
+}
+
+// 食事情報をDOMに追加
+function addMealToDom(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  console.log(meal)
+
+  single_mealEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map((ing) => {
+            return `<li>${ing}</li>`
+          }).join("")}
+        </ul>
+      </div>
+    </div>
+  `
+}
+
 // イベントリスナー
 submit.addEventListener("submit", searchMeal);
+
+mealsEl.addEventListener("click", (e) => {
+  const path = e.composedPath();
+  const mealInfo = path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains("meal-info");
+    } else {
+      return false;
+    }
+  });
+
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute("data-mealid");
+    getMealById(mealID);
+  }
+});
